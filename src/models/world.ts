39ed817898884cc22b790de7a00c9edb5ac42a98
Francisco Silva <franciscocrosilva@gmail.com>
2020-11-10 13:43:24 +0000

    🥳 World optimization

--- src/models/world.ts
+++ src/models/world.ts
@@ -2,22 +2,18 @@ import { Position } from "./position";
 
 export class World {
 
-    private emptyPositions: Array<Position>;
+    private emptyPositions: Map<string, string>;
 
     constructor() {
-        this.emptyPositions = [];
+        this.emptyPositions = new Map();
     }
 
     public containsPokemon(position: Position): boolean {
-        return this.emptyPositions.findIndex(
-            value =>
-                value.xPosition === position.xPosition && value.yPosition === position.yPosition
-        ) === -1;
+        return !this.emptyPositions.has(`${position.xPosition};${position.yPosition}`);
     }
 
     public removePokemon(position: Position) {
-        const emptyPos: Position = new Position(position.xPosition, position.yPosition);
-        this.emptyPositions.push(emptyPos);
+        this.emptyPositions.set(`${position.xPosition};${position.yPosition}`, 'empty');
     }
 
 }
